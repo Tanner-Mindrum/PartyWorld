20050e1f2a66a9d72214116863939a93da758e08
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-04-25 17:04:09 -0700

    Updated files 4/25

--- src/PartyRoom.java
+++ src/PartyRoom.java
@@ -4,6 +4,8 @@ public abstract class PartyRoom {
     private double cost; //cost per hour to rent room
     private String upgrades; //available upgrades for room
     //private MealPlan meals;
+    private PartyGoer partyGoerInfo;
+    private boolean reservationMade;
 
 
     /**
@@ -14,6 +16,8 @@ public abstract class PartyRoom {
         this.capacity = 0;
         this.cost = 0;
         this.upgrades = "N/A";
+        this.partyGoerInfo = null;
+        this.reservationMade = false;
     }
 
     /**
@@ -32,29 +36,67 @@ public abstract class PartyRoom {
         this.description = description;
     }
 
-
+    /**
+     * Gets the possible capacity of a room
+     * @return - an int representing how many people can fit in the room
+     */
     public int getCapacity() {
         return capacity;
     }
 
+    /**
+     * Sets the possible capacity of a room
+     * @param capacity - an int representing the new amount of people that can fit in a room
+     */
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
 
+    /**
+     * Gets the current cost of a room
+     * @return - a double representing the cost of a room per hour
+     */
     public double getCost() {
         return cost;
     }
 
+    /**
+     * Sets the current cost of a room
+     * @param cost - a double representing the new cost per hour of a room
+     */
     public void setCost(int cost) {
         this.cost = cost;
     }
 
+    /**
+     * Gets the available upgrades for a room
+     * @return - a string representing the possible
+     */
     public String getUpgrades() {
         return upgrades;
     }
 
     public void setUpgrades(String upgrades) {
         this.upgrades = upgrades;
+    }
+
+    public void setReservation(PartyGoer currentPartyGoer) {
+
+    }
+
+    public void cancelReservation(PartyGoer currentPartyGoer) {
+
+    }
+
+    public String notifyPartyGoer() {
+        //Notifying whether they set or cancel
+        if (reservationMade) {
+            //if room is full, add to wait list
+            return "Your reservation has been set!";
+        }
+        else {
+            return "Your reservation has been cancelled.";
+        }
     }
 
     public abstract double calculateCost();
