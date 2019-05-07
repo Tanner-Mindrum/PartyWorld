f73c8714023baba7144fd571bd4ef44c394aa97e
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-05-07 11:50:35 -0700

    Trying to fix merge error

--- src/AquaWorld/Client.java
+++ src/AquaWorld/Client.java
@@ -2,20 +2,35 @@ package AquaWorld;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class Client {
-    
+
     public static void main(String[] args) {
         System.out.println("Test");
         ArrayList<String> reservationDates = new ArrayList<>();
         ArrayList<Integer> reservationTimes = new ArrayList<>();
         PartyGoer partyGoer = null;
         PartyRoom partyRoom = null;
+        Pizza pizza1 = new Pizza();
+        pizza1 = new Cheese(pizza1);
+        pizza1 = new Pepperoni(pizza1);
+        pizza1 = new Ham(pizza1);
+        pizza1 = new Pineapple(pizza1);
+        System.out.println(pizza1.getDescription());
+
+        PartyRoom partyRoom1 = AquaWorldRoom.getInstance();
+        partyRoom1 = new Projector(partyRoom1);
+        partyRoom1 = new TowelRentals(partyRoom1);
+        System.out.println(partyRoom1.getDescription());
+        PartyRoom partyRoom2 = new mediumPartyRoom();
+        partyRoom2 = new PartyFavorBag(partyRoom2);
+        partyRoom2 = new Projector(partyRoom2);
+        System.out.println(partyRoom2.getDescription());
+
         //AquaWorldRoom aqua = AquaWorldRoom.getInstance();
 //        AquaWorldRoom newAqua = AquaWorldRoom.getInstance();
 //        aqua.addRestrictions("You must wear bathing suits to the pool");
-//        System.out.println(newAqua.getRestrictions());  
-        
+//        System.out.println(newAqua.getRestrictions());
+
         //Testing PartyRoomFactory class
 //        PartyRoomFactory roomFactory = new PartyRoomFactory();
 //        roomFactory.aquaFactory();
@@ -26,6 +41,7 @@ public class Client {
 //       // medRoom2.setDescription("Second object");
 //        System.out.println(medRoom1.getDescription());
 //        System.out.println(medRoom2.getDescription());
+        MainFrame frame = new MainFrame();
 
         Scanner in = new Scanner(System.in);
         System.out.println("BUSINESS HOURS: 9:00 AM to 12:00 AM");
@@ -75,33 +91,33 @@ public class Client {
 
             if (reservationDates.size() != 0) {
 //                while (true) {
-                    if (reservationDates.contains(date)) {
-                        ArrayList<String> takenDates = new ArrayList<>();
-                        ArrayList<Integer> takenTimes = new ArrayList<>();
-                        boolean roomTaken = false;
-                        for (int i = 0; i < reservationDates.size(); i++) {
-                            if (reservationDates.get(i).equals(date)) {
-                                takenDates.add(reservationDates.get(i));
-                                takenTimes.add(reservationTimes.get(i));
-                                takenTimes.add(reservationTimes.get(i + 1));
-                            }
-                        }
-                        for (int i = 0; i < reservationTimes.size() - 1; i++) {
-                            if (milStartTimeInt < reservationTimes.get(i) && milStartTimeInt < reservationTimes.get(i + 1)) {
-                                roomTaken = true;
-                                break;
-                            }
-                        }
-
-                        //Observer begin
-                        if (roomTaken) {
-                            System.out.println("You have been added to a wait list");
-                            partyRoom.getNotification();
-                        }
-                        else {
-                            System.out.println("You have been added to a wait list");
+                if (reservationDates.contains(date)) {
+                    ArrayList<String> takenDates = new ArrayList<>();
+                    ArrayList<Integer> takenTimes = new ArrayList<>();
+                    boolean roomTaken = false;
+                    for (int i = 0; i < reservationDates.size(); i++) {
+                        if (reservationDates.get(i).equals(date)) {
+                            takenDates.add(reservationDates.get(i));
+                            takenTimes.add(reservationTimes.get(i));
+                            takenTimes.add(reservationTimes.get(i + 1));
                         }
                     }
+                    for (int i = 0; i < reservationTimes.size() - 1; i++) {
+                        if (milStartTimeInt < reservationTimes.get(i) && milStartTimeInt < reservationTimes.get(i + 1)) {
+                            roomTaken = true;
+                            break;
+                        }
+                    }
+
+                    //Observer begin
+                    if (roomTaken) {
+                        System.out.println("You have been added to a wait list");
+                        partyRoom.getNotification();
+                    }
+                    else {
+                        System.out.println("You have been added to a wait list");
+                    }
+                }
 //                    for (int i = 0; i < reservationTimes.size() - 1; i++) {
 //                        if (milStartTimeInt < reservationTimes.get(i) && milStartTimeInt < reservationTimes.get(i + 1)) {
 //                            if (reservationDates.get(i).equals(date)) {
@@ -155,3 +171,4 @@ public class Client {
         }
     }
 }
+
