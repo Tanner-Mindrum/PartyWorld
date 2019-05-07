ccf3fa77e80bfe1952911f43d6287675e2f802a4
justin <justinquarress@gmail.com>
2019-05-07 13:13:09 -0700

    finished the backend

--- src/AquaWorld/Client.java
+++ src/AquaWorld/Client.java
@@ -39,21 +39,21 @@ public class Client {
         tanner.updateInfo(tanner.getName() + partyRoom1.getNotification());
         System.out.println(tanner.getInfo());
 
-        //AquaWorldRoom aqua = AquaWorldRoom.getInstance();
-//        AquaWorldRoom newAqua = AquaWorldRoom.getInstance();
-//        aqua.addRestrictions("You must wear bathing suits to the pool");
-//        System.out.println(newAqua.getRestrictions());
+        AquaWorldRoom aqua = AquaWorldRoom.getInstance();
+        AquaWorldRoom newAqua = AquaWorldRoom.getInstance();
+        aqua.addRestrictions("You must wear bathing suits to the pool");
+        System.out.println(newAqua.getRestrictions());
 
         //Testing PartyRoomFactory class
-//        PartyRoomFactory roomFactory = new PartyRoomFactory();
-//        roomFactory.aquaFactory();
-//        roomFactory.aquaFactory();
-//        mediumPartyRoom medRoom1 = roomFactory.mediumFactory();
-//        mediumPartyRoom medRoom2 = roomFactory.mediumFactory();
-//        medRoom1.setDescription("First object");
-//       // medRoom2.setDescription("Second object");
-//        System.out.println(medRoom1.getDescription());
-//        System.out.println(medRoom2.getDescription());
+        PartyRoomFactory roomFactory = new PartyRoomFactory();
+        roomFactory.aquaFactory();
+        roomFactory.aquaFactory();
+        mediumPartyRoom medRoom1 = roomFactory.mediumFactory();
+        mediumPartyRoom medRoom2 = roomFactory.mediumFactory();
+        medRoom1.setDescription("First object");
+        medRoom2.setDescription("Second object");
+        System.out.println(medRoom1.getDescription());
+        System.out.println(medRoom2.getDescription());
         MainFrame frame = new MainFrame();
 
         Scanner in = new Scanner(System.in);
