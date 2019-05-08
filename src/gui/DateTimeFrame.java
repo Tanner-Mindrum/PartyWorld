0429853b185f866d64ea0acb4ebecaa392e48dd4
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-05-08 13:56:27 -0700

    Updated main frame

--- src/gui/DateTimeFrame.java
+++ src/gui/DateTimeFrame.java
@@ -1,13 +1,58 @@
 package gui;
 
 import javax.swing.*;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateTimeFrame extends JFrame {
 
     //Date/Time info
+    private JLabel monthLabel;
+    private JLabel dayLabel;
+    private JLabel yearLabel;
+    private JLabel timeLabel;
+    private JSpinner dateTimeSpinner;
+    private Calendar calender;
+    private Date date;
+    String[] monthStrings;
 
+    public DateTimeFrame() {
+        this.setTitle("Select Date/Time");
+        this.monthStrings = getMonthStrings();
+    }
 
-    private DateTimeFrame() {
+    /**
+     * Return the formatted text field used by the editor, or
+     * null if the editor doesn't descend from JSpinner.DefaultEditor.
+     */
+    public JFormattedTextField getTextField(JSpinner spinner) {
+        JComponent editor = spinner.getEditor();
+        if (editor instanceof JSpinner.DefaultEditor) {
+            return ((JSpinner.DefaultEditor)editor).getTextField();
+        } else {
+            System.err.println("Unexpected editor type: "
+                    + spinner.getEditor().getClass()
+                    + " isn't a descendant of DefaultEditor");
+            return null;
+        }
+    }
 
+    /**
+     * DateFormatSymbols returns an extra, empty value at the
+     * end of the array of months.  Remove it.
+     */
+    static protected String[] getMonthStrings() {
+        String[] months = new java.text.DateFormatSymbols().getMonths();
+        int lastIndex = months.length - 1;
+
+        if (months[lastIndex] == null
+                || months[lastIndex].length() <= 0) { //last item empty
+            String[] monthStrings = new String[lastIndex];
+            System.arraycopy(months, 0,
+                    monthStrings, 0, lastIndex);
+            return monthStrings;
+        } else { //last item not empty
+            return months;
+        }
     }
 }
