b05bba7dae9020e3fbbe2d07a84b3f7c1aab6d8b
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-05-08 22:18:20 -0700

    Updated files

--- src/AquaWorld/karaokeLounge.java
+++ src/AquaWorld/karaokeLounge.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class karaokeLounge extends PartyRoom {
 	private karaokeLounge partyRoom;
-//	private String Description = "Enclosed karaoke lounge with karaoke machine";
+	private String Description = "Enclosed karaoke lounge with karaoke machine";
 	ArrayList<PartyGoer> waitList = new ArrayList<PartyGoer>();
 
 
@@ -17,24 +17,12 @@ public class karaokeLounge extends PartyRoom {
 		return null;
 	}
 
-//	public String getDescription() {
-//		return Description;
-//	}
+	public String getDescription() {
+		return Description;
+	}
 	
 	public void addWaitList(PartyGoer partyGoer) {
 		waitList.add(partyGoer);
-	}
-
-	public String toString() {
-		return "Capacity: 10 people\nCost: $30/hr\nIncluded in cost:\n\t-Basic Meal Plan\n\t-Table & Chair Set-up\n"
-//				+ "Upgrades Available:\n\t-Upgrade Meal Plan\tCost: $(meal plan cost)\n\t-Party favor bags\t Cost: $5 per bag\n\t"
-//				+ "-Projector\t\tCost: $10/hour\n\t-Party Decorations & set-Up       Cost: $100\n\t"
-//				+ "     Themes: Hawaiian, Sea Life, Jungle, Space, or Modern";;
-
-//		return "Capacity: 30 people\nCost: $150/hr\nIncluded in cost:\n\t-Basic Meal Plan\n\t-Table & Chair Set-up\n"
-//				+ "Upgrades Available:\n\t-Upgrade Meal Plan\tCost: $(meal plan cost)\n\t-Party favor bags\t Cost: $5 per bag\n\t"
-//				+ "-Projector\t\tCost: $10/hour\n\t-Party Decorations & set-Up       Cost: $100\n\t"
-//				+ "     Themes: Hawaiian, Sea Life, Jungle, Space, or Modern";
 	}
 
 }
--- src/gui/DateTimeFrame.java
+++ src/gui/DateTimeFrame.java
@@ -1,6 +1,10 @@
 package gui;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,14 +15,115 @@ public class DateTimeFrame extends JFrame {
     private JLabel dayLabel;
     private JLabel yearLabel;
     private JLabel timeLabel;
-    private JSpinner dateTimeSpinner;
+    private JSpinner monthSpinner;
+    private JSpinner daySpinner;
+    private JSpinner yearSpinner;
     private Calendar calender;
     private Date date;
     String[] monthStrings;
+    ArrayList<Integer> dayInts;
+    private JTextField textField;
+    private JPanel panel;
+
+    private static final int FRAME_WIDTH = 500;
+    private static final int FRAME_HEIGHT = 400;
 
     public DateTimeFrame() {
         this.setTitle("Select Date/Time");
         this.monthStrings = getMonthStrings();
+        this.dayInts = new ArrayList<>();
+        setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        createComponents();
+    }
+
+    public void createComponents() {
+        panel = new JPanel();
+        monthLabel = new JLabel("Month: ");
+        dayLabel = new JLabel("Day: ");
+        yearLabel = new JLabel("Year: ");
+
+        //Month spinner
+        SpinnerListModel monthModel = new SpinnerListModel(this.monthStrings);
+        monthSpinner = new JSpinner(monthModel);
+        Dimension dimension = monthSpinner.getPreferredSize();
+        dimension.width = 82;
+        monthSpinner.setPreferredSize(dimension);
+
+        //Make 31 days
+        ArrayList<Integer> dayInts = new ArrayList<>();
+        for (int i = 1; i < 32; i++) {
+            dayInts.add(i);
+        }
+
+        //Make years
+        ArrayList<Integer> yearInts = new ArrayList<>();
+        for (int i = 2019; i <= 2021; i++) {
+            yearInts.add(i);
+        }
+
+        //Make day spinner
+        SpinnerListModel dayModel = new SpinnerListModel(dayInts);
+        daySpinner = new JSpinner(dayModel);
+        Dimension dimension2 = daySpinner.getPreferredSize();
+        dimension2.width = 82;
+        daySpinner.setPreferredSize(dimension2);
+
+        SpinnerListModel yearModel = new SpinnerListModel(yearInts);
+        yearSpinner = new JSpinner(yearModel);
+        Dimension dimension3 = yearSpinner.getPreferredSize();
+        dimension.width = 82;
+        yearSpinner.setPreferredSize(dimension3);
+
+        panel.add(monthLabel);
+        panel.add(monthSpinner);
+        panel.add(dayLabel);
+        panel.add(daySpinner);
+        this.add(panel);
+
+        monthSpinner.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                String value = monthSpinner.getValue().toString();
+                if (value.equals("February")) {
+                    panel.remove(daySpinner);
+                    panel.remove(yearLabel);
+                    panel.remove(yearSpinner);
+                    panel.repaint();
+
+
+                    SpinnerListModel dayModel = new SpinnerListModel(dayInts.subList(0, 28));
+                    daySpinner = new JSpinner(dayModel);
+                    Dimension dimension = daySpinner.getPreferredSize();
+                    dimension.width = 82;
+                    daySpinner.setPreferredSize(dimension);
+
+                    panel.add(daySpinner);
+                    panel.add(yearLabel);
+                    panel.add(yearSpinner);
+                    panel.revalidate();
+                }
+                else {
+                    panel.remove(daySpinner);
+                    panel.remove(yearLabel);
+                    panel.remove(yearSpinner);
+                    panel.repaint();
+
+                    SpinnerListModel dayModel = new SpinnerListModel(dayInts);
+                    daySpinner = new JSpinner(dayModel);
+                    Dimension dimension = daySpinner.getPreferredSize();
+                    dimension.width = 82;
+                    daySpinner.setPreferredSize(dimension);
+
+                    panel.add(daySpinner);
+                    panel.add(yearLabel);
+                    panel.add(yearSpinner);
+                    panel.revalidate();
+                }
+            }
+        });
+
+        panel.add(yearLabel);
+        panel.add(yearSpinner);
     }
 
     /**
