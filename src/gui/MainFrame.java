3e3ef29658f74a414f44844550ccbaac80759897
justin <justinquarress@gmail.com>
2019-05-08 22:24:41 -0700

    nothing

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -302,8 +302,8 @@ public class MainFrame extends JFrame {
 		}
 	}
 	
-	public static void main(String[] args
-			) {
-		MainFrame f = new MainFrame();
-	}
+//	public static void main(String[] args
+//			) {
+//		MainFrame f = new MainFrame();
+//	}
 }
