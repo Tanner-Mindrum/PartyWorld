17927a9040a4084de5f1ffceb40eb01b39637134
TymeeK <tymeekong562@yahoo.com>
2019-05-10 17:19:48 -0700

    Finished implementation of bronze and silver meal plans

--- src/AquaWorld/bronzeMeal.java
+++ src/AquaWorld/bronzeMeal.java
@@ -1,5 +1,18 @@
 package AquaWorld;
 
-public class bronzeMeal {
+public class bronzeMeal extends MealPlan {
+
+    @Override
+    public String getDescription() {
+        description = "Included in meal:\n\t-3 XL 2 Topping Gourmet Pizzas\n\t-5 2L Soda Bottles\n\t-Salad or bread-sticks\nCost: $75";
+        return description;
+    }
+
+    @Override
+    public double getCost() {
+        cost = 75;
+        return cost; 
+    }
+
 
 }
--- src/AquaWorld/silverMeal.java
+++ src/AquaWorld/silverMeal.java
@@ -1,5 +1,17 @@
 package AquaWorld;
 
-public class silverMeal {
+public class silverMeal extends MealPlan{
+
+    @Override
+    public String getDescription() {
+        description = "Included in meal:\n\t-3 XL 3 Topping Gourmet Pizzas\n\t-5 2L Soda Bottles\n\t-Salad\n\t-Bread-sticks\nCost: $90";
+        return description;
+    }
+
+    @Override
+    public double getCost() {
+        cost = 90;
+        return cost;
+    }
 
 }
