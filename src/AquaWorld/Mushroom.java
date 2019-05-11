91259bb94e189ab0fc290f58a7bb44a664de3ea5
LAPTOP-V5E93NNM\steve <stevencenteno13@gmail.com>
2019-05-11 16:29:26 -0700

    java docs

--- src/AquaWorld/Mushroom.java
+++ src/AquaWorld/Mushroom.java
@@ -1,13 +1,28 @@
 package AquaWorld;
 
+/**
+ * 
+ * @author steven, tanner, tymee, justin
+ *
+ *
+ * adds mushrooms to the pizza as a topping
+ */
 public class Mushroom extends PizzaDecorator {
 	
 	Pizza pizza;
 	
+	/**
+	 * constructor for the pizza that adds mushrooms to the pizza
+	 * @param pizza to add the mushrooms to
+	 */
 	public Mushroom(Pizza pizza) {
 		this.pizza = pizza; 
 	}
 	
+	/**
+	 * gets the description of the pizza
+	 * @return the description of the pizza as a string
+	 */
 	public String getDescription() {
 		return pizza.getDescription() + ", Mushroom";
 	}
--- src/AquaWorld/Onion.java
+++ src/AquaWorld/Onion.java
@@ -1,12 +1,25 @@
 package AquaWorld;
 
+/**
+ * 
+ * @author steven, tymee, justin, tanner
+ *
+ */
 public class Onion extends PizzaDecorator  {
 	Pizza pizza;
 	
+	/**
+	 * constructor that adds onions to the pizza
+	 * @param pizza that will have onions added to it
+	 */
 	public Onion(Pizza pizza) {
 		this.pizza = pizza; 
 	}
 	
+	/**
+	 * gets the description of the pizza
+	 * @return the pizza's description as a String
+	 */
 	public String getDescription() {
 		return pizza.getDescription() + ", Onion";
 	}
