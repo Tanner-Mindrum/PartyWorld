23ca20f8650950bf6f6d791271700602181669a9
TymeeK <tymeekong562@yahoo.com>
2019-05-14 17:48:43 -0700

    Formatted it to look nicer

--- src/gui/NewReservationFrame.java
+++ src/gui/NewReservationFrame.java
@@ -237,10 +237,8 @@ public class NewReservationFrame {
 
     public void currentDate() {
         
-        
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.MONTH, 1);
-        
         System.out.println(calendar.get(Calendar.MONTH) + "/"  + calendar.get(Calendar.DAY_OF_MONTH)  + "/" + calendar.get(Calendar.YEAR));
         
     }
